e1ea340d52fa229ec82abbb7aab392f5e1ae85b2
harism <harism@gmail.com>
2012-06-13 02:46:01 +0300

    Some span examples added.

--- src/fi/harism/lucidchat/LucidChatActivity.java
+++ src/fi/harism/lucidchat/LucidChatActivity.java
@@ -2,14 +2,19 @@ package fi.harism.lucidchat;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.URLSpan;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.TextView.BufferType;
+import android.widget.Toast;
 
 public class LucidChatActivity extends Activity {
 	
@@ -29,8 +34,14 @@ public class LucidChatActivity extends Activity {
         	span.setSpan(new ForegroundColorSpan(0xFF4CBAED), 0, 5, 0);
         	span.setSpan(new ForegroundColorSpan(0xFF17AEF4), 6, 11, 0);
         	span.setSpan(new ForegroundColorSpan(0xFFD0D0D0), 12, 20, 0);
-        	span.setSpan(new ForegroundColorSpan(0xFF63cb63), 20, 28, 0);
+        	span.setSpan(new ForegroundColorSpan(0xFF63CB63), 20, 28, 0);
         	
+        	span.setSpan(new URLSpan("tel:+358-123 123 123"), 29, 35, 0);        	
+        	span.setSpan(new URLSpan("http://www.hs.fi"), 37, 45, 0);
+        	span.setSpan(new URLSpan("mailto:harism@gmail.com"), 47, 55, 0);
+        	span.setSpan(new ForegroundColorSpan(0xFF6AD46A), 47, 55, 0);
+        	
+        	tv.setMovementMethod(LinkMovementMethod.getInstance());        	
         	tv.setText(span, BufferType.SPANNABLE);
         	
         	chat.addView(view);
